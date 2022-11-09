fd31eb056adf6b3ce10b9b669ea13a4c6bf82cc7
felipemike <felipeclemente2012@gmail.com>
2022-11-09 01:07:33 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import {nanoid} from 'nanoid'
+import './style.css'
+
+const bnt = document.getElementById('btn')
+
+const pass = document.getElementById('pass')
+
+bnt.addEventListener('click', () => {
+  const randomPass = nanoid();
+  pass.innerHTML = randomPass;
+});
